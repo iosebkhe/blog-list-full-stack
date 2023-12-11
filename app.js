a30f5b5ee120542e9466434e784882148ed38c56
iosebkhe <ioseb.khetsuriani@yahoo.com>
2023-12-12 01:02:03 +0400

    added configuration for cypress e2e testing

--- app.js
+++ app.js
@@ -32,6 +32,11 @@ app.use('/api/blogs', blogListRouter);
 app.use('/api/users', userRouter);
 app.use('/api/login', loginRouter);
 
+if (process.env.NODE === "test") {
+  const testingRouter = require("./controllers/testing");
+  app.use("/api/testing", testingRouter);
+}
+
 app.use(middleware.errorHandler);
 
 module.exports = app;
