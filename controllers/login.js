86e42628c8dbd1a87a5045ecf5762b6a2d5ba5d6
iosebkhe <khetsurianiioseb@gmail.com>
2023-12-18 21:32:15 +0400

    added small formatting

--- controllers/login.js
+++ controllers/login.js
@@ -9,27 +9,30 @@ loginRouter.post("/", async (request, response, next) => {
 
     const user = await User.findOne({ username });
 
-    const passwordCorrect = user === null
-      ? false
-      : await bcrypt.compare(password, user.passwordHash);
+    const passwordCorrect =
+      user === null ? false : await bcrypt.compare(password, user.passwordHash);
 
     if (!(user && passwordCorrect)) {
-      return response.status(401).json({ error: 'invalid username or password' });
+      return response
+        .status(401)
+        .json({ error: "invalid username or password" });
     }
 
     const infoForToken = {
       username: user.username,
-      id: user.id
+      id: user.id,
     };
 
-    const token = jwt.sign(infoForToken, process.env.SECRET, { expiresIn: 60 * 60 });
+    const token = jwt.sign(infoForToken, process.env.SECRET, {
+      expiresIn: 60 * 60,
+    });
 
-    response.status(201).json({ token, username: user.username, name: user.name });
-
+    response
+      .status(201)
+      .json({ token, username: user.username, name: user.name, id: user.id });
   } catch (error) {
     next(error);
   }
 });
-
 
 module.exports = loginRouter;
